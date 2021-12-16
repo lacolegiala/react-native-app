2fd05548648f7ba21666e9fecf96a93d25308175
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-12-16 09:54:52 +0200

    Finish repo item rendering

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 const RepositoryItem = ({item}) => {
   return (
-    <Text>
-      Full name: {item.fullName}
-    </Text>
+    <View>
+      <Text>
+        Full name: {item.fullName}
+      </Text>
+      <Text>
+        Description: {item.description}
+      </Text>
+      <Text>
+        Language: {item.language}
+      </Text>
+      <Text>
+        Stars: {item.stargazersCount}
+      </Text>
+      <Text>
+        Forks: {item.forksCount}
+      </Text>
+      <Text>
+        Reviews: {item.reviewCount}
+      </Text>
+      <Text>
+        Rating: {item.ratingAverage}
+      </Text>
+    </View>
   );
 };
 
