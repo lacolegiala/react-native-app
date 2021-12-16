8f500b402a806e025b0dc4a063a46a60829baff6
Sofia Riekkola <sofia.riekkola@gmail.com>
2021-12-16 17:55:55 +0200

    Add color and text to app bar

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
+import Text from '../components/Text';
 import Constants from 'expo-constants';
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
-    // ...
+    backgroundColor: '#25292C'
   },
-  // ...
+  text: {
+    color: 'white'
+  }
 });
 
 const AppBar = () => {
-  return <View style={styles.container}>{/* ... */}</View>;
+  return <View style={styles.container}><Text color='appBarText'>Repositories</Text></View>;
 };
 
 export default AppBar;
