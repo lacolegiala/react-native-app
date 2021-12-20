8b5939b461d7933896a3bef6c5d7db4abd59297e
Sofia Riekkola <sofiariekkola@Sofia-MacBook-Pro.local>
2021-12-20 20:25:16 +0200

    Reduce opacity and make app bar pressable

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import Text from '../components/Text';
 import Constants from 'expo-constants';
+import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
 
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#25292C'
+    backgroundColor: '#25292C',
+    display: 'flex',
+    opacity: 0.9
   },
   text: {
     color: 'white'
@@ -14,7 +17,13 @@ const styles = StyleSheet.create({
 });
 
 const AppBar = () => {
-  return <View style={styles.container}><Text color='appBarText' fontWeight='bold'>Repositories</Text></View>;
+  return (
+    <Pressable onPress={console.log('aaa')}>
+      <View style={styles.container}>
+        <Text color='appBarText' fontWeight='bold'>Repositories</Text>
+      </View>
+    </Pressable>
+  );
 };
 
 export default AppBar;
