317b93d7885b40e10bb9d5b6c5f90dcd0af8d5bb
Sofia Riekkola <sofiariekkola@Sofia-MBP.home>
2021-12-21 13:55:48 +0200

    Add padding

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -7,6 +7,7 @@ import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
 const styles = StyleSheet.create({
   container: {
     paddingTop: Constants.statusBarHeight,
+    paddingBottom: 16,
     backgroundColor: '#25292C',
     display: 'flex',
     opacity: 0.9
