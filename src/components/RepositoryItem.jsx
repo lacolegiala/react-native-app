bcc412b78bfeb30829ec8b7c673ae74b8f782732
Sofia Riekkola <sofiariekkola@Sofia-MacBook-Pro.local>
2021-12-22 12:14:16 +0200

    Put stuff in rows

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,30 +1,47 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  pieceOfInfo: {
+    flexDirection: 'row'
+  }
+})
 
 const RepositoryItem = ({item}) => {
   return (
     <View>
-      <Text>
-        Full name: {item.fullName}
-      </Text>
-      <Text>
-        Description: {item.description}
-      </Text>
-      <Text>
-        Language: {item.language}
-      </Text>
-      <Text>
-        Stars: {item.stargazersCount}
-      </Text>
-      <Text>
-        Forks: {item.forksCount}
-      </Text>
-      <Text>
-        Reviews: {item.reviewCount}
-      </Text>
-      <Text>
-        Rating: {item.ratingAverage}
-      </Text>
+      <View style={styles.pieceOfInfo}>
+        <Image style={{width: 50, height: 50}} source={{uri: item.ownerAvatarUrl}}></Image>
+        <View>
+          <Text>
+            {item.fullName}
+          </Text>
+          <Text>
+            {item.description}
+          </Text>
+          <Text>
+            {item.language}
+          </Text>
+        </View>
+      </View>
+      <View style={styles.pieceOfInfo}>
+        <View>
+          <Text>{item.stargazersCount}</Text>
+          <Text>Stars</Text>
+        </View>
+        <View>
+          <Text>{item.forksCount}</Text>
+          <Text>Forks</Text>
+        </View>
+        <View>
+          <Text>{item.reviewCount}</Text>
+          <Text>Reviews</Text>
+        </View>
+        <View>
+          <Text>{item.ratingAverage}</Text>
+          <Text>Rating</Text>
+        </View>
+      </View>
     </View>
   );
 };
