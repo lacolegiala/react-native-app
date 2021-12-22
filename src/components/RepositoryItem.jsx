c3165a260d57cdfde8469e1cdfe0184ca19c69f8
Sofia Riekkola <sofiariekkola@Sofia-MacBook-Pro.local>
2021-12-22 19:25:41 +0200

    Add a basic version of number fornat

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -45,8 +45,20 @@ const styles = StyleSheet.create({
   },
   stat: {
     alignItems: 'center'
+  },
+  statText: {
+    marginTop: 4
   }
 })
+
+const formatNumber = (stat) => {
+  if (stat < 1000) {
+    return stat
+  }
+  else {
+    return stat / 1000
+  }
+}
 
 const RepositoryItem = ({item}) => {
   return (
@@ -71,20 +83,20 @@ const RepositoryItem = ({item}) => {
       </View>
       <View style={styles.statWrapper}>
         <View style={styles.stat}>
-          <Text fontWeight='bold'>{item.stargazersCount}</Text>
-          <Text color='textSecondary'>Stars</Text>
+          <Text fontWeight='bold'>{formatNumber(item.stargazersCount)}</Text>
+          <Text style={styles.statText} color='textSecondary'>Stars</Text>
         </View>
         <View style={styles.stat}>
-          <Text fontWeight='bold'>{item.forksCount}</Text>
-          <Text color='textSecondary'>Forks</Text>
+          <Text fontWeight='bold'>{formatNumber(item.forksCount)}</Text>
+          <Text style={styles.statText} color='textSecondary'>Forks</Text>
         </View>
         <View style={styles.stat}>
-          <Text fontWeight='bold'>{item.reviewCount}</Text>
-          <Text color='textSecondary'>Reviews</Text>
+          <Text fontWeight='bold'>{formatNumber(item.reviewCount)}</Text>
+          <Text style={styles.statText} color='textSecondary'>Reviews</Text>
         </View>
         <View style={styles.stat}>
           <Text fontWeight='bold'>{item.ratingAverage}</Text>
-          <Text color='textSecondary'>Rating</Text>
+          <Text style={styles.statText} color='textSecondary'>Rating</Text>
         </View>
       </View>
     </View>
