2d1862c2af142f2d05afa2568d116c0f8f88cccf
Sofia Riekkola <sofiariekkola@Sofia-MacBook-Pro.local>
2021-12-23 14:46:59 +0200

    Make app use native router

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { NativeRouter } from 'react-router-native';
+import { StatusBar } from 'expo-status-bar';
 
 import Main from './src/components/Main';
 
 const App = () => {
-  return <Main />;
+  return (
+    <>
+      <NativeRouter>
+        <Main />
+      </NativeRouter>
+      <StatusBar style="auto" />
+    </>
+  )
 };
 
 export default App;
