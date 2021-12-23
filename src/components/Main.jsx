73e0d847226fa89877eb8750ad4f2d0681bb14a3
Sofia Riekkola <sofiariekkola@Sofia-MacBook-Pro.local>
2021-12-23 14:57:58 +0200

    Add a route for sign in

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,6 +4,7 @@ import { Route, Switch, Redirect } from 'react-router-native';
 
 import RepositoryList from './RepositoryList';
 import AppBar from './AppBar';
+import SignIn from './SignIn';
 
 const styles = StyleSheet.create({
   container: {
@@ -20,6 +21,9 @@ const Main = () => {
       <Switch>
         <Route path="/" exact>
           <RepositoryList />
+        </Route>
+        <Route path="/signin">
+          <SignIn />
         </Route>
         <Redirect to="/" />
       </Switch>
