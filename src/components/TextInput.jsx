9d99fe3f1d31b0f0e7b934c1439267a198a23188
Sofia Riekkola <sofiariekkola@Sofia-MacBook-Pro.local>
2022-01-03 17:42:25 +0200

    Style sign in

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { TextInput as NativeTextInput, StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	textInput: {
+		fontSize: 20,
+		borderWidth: 0.5,
+		borderColor: '#B7BAB7'
+	}
+});
 
 const TextInput = ({ style, error, ...props }) => {
-  const textInputStyle = [style];
+  const textInputStyle = [style, styles.textInput];
 
   return <NativeTextInput style={textInputStyle} {...props} />;
 };
