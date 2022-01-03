3acfbd62331847cf0d0afaa3a6ac68bce07be1d4
Sofia Riekkola <sofiariekkola@Sofia-MacBook-Pro.local>
2022-01-03 15:33:00 +0200

    Give fields initial values

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -3,9 +3,19 @@ import { View } from 'react-native'
 import FormikTextInput from './FormikTextInput';
 import { Formik } from 'formik'
 
+const onSubmit = (values) => {
+  console.log(values);
+};
+
 const SignIn = () => {
   return (
-    <Formik>
+    <Formik
+      initialValues={{
+        username: '',
+        password: ''
+      }}
+      onSubmit={onSubmit}
+    >
       <View>
         <FormikTextInput name='username' placeholder='Username' />
         <FormikTextInput name='password' placeholder='Password' />
