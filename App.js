8086f389acca9609b1f38eceb0ed3c2f54133769
Sofia Riekkola <sofiariekkola@Sofia-MacBook-Pro-2.local>
2022-09-03 11:48:24 +0300

    Save token

--- App.js
+++ App.js
@@ -3,9 +3,12 @@ import { NativeRouter } from 'react-router-native';
 import { ApolloProvider } from '@apollo/client';
 
 import Main from './src/components/Main';
+import AuthStorage from './src/utils/authStorage';
 import createApolloClient from './src/utils/apolloClient';
 
-const apolloClient = createApolloClient();
+const authStorage = new AuthStorage();
+
+const apolloClient = createApolloClient(authStorage);
 
 const App = () => {
   return (
--- src/utils/apolloClient.ts
+++ src/utils/apolloClient.ts
@@ -1,20 +1,34 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { Constants } from 'react-native-unimodules';
+import { setContext } from '@apollo/client/link/context';
+import AuthStorage from './authStorage';
 
 const httpLink = createHttpLink({
   // Replace the IP address part with your own IP address!
   uri: Constants.manifest.extra.apolloUri,
 });
 
-const createApolloClient = () => {
-  try {
-    return new ApolloClient({
-      link: httpLink,
-      cache: new InMemoryCache(),
-    });
-  } catch (error) {
-    console.log(error)
-  }
+const createApolloClient = (authStorage: AuthStorage) => {
+  const authLink = setContext(async (_, { headers }) => {
+    try {
+      const accessToken = await authStorage.getAccessToken();
+      return {
+        headers: {
+          ...headers,
+          authorization: accessToken ? `Bearer ${accessToken}` : '',
+        },
+      };
+    } catch (e) {
+      console.log(e);
+      return {
+        headers,
+      };
+    }
+  });
+  return new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
 };
 
 export default createApolloClient;
